5620674305d00e8bd8fe46d9a4bb698ea4cbe80a
Hasnain Irfan <hasnain@getweys.com>
2023-08-07 22:37:47 +0500

    Main

---
+++ src/authContext.jsx
@@ -0,0 +1,62 @@
+import React, { useReducer } from "react";
+import MkdSDK from "./utils/MkdSDK";
+
+export const AuthContext = React.createContext();
+
+const initialState = {
+  isAuthenticated: false,
+  user: null,
+  token: null,
+  role: null,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        ...state,
+      };
+    case "LOGOUT":
+      localStorage.clear();
+      return {
+        ...state,
+        isAuthenticated: false,
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+let sdk = new MkdSDK();
+
+export const tokenExpireError = (dispatch, errorMessage) => {
+  const role = localStorage.getItem("role");
+  if (errorMessage === "TOKEN_EXPIRED") {
+    dispatch({
+      type: "Logout",
+    });
+    window.location.href = "/" + role + "/login";
+  }
+};
+
+const AuthProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  React.useEffect(() => {
+    //TODO
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        state,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
